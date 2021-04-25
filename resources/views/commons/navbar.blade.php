233526e04c3d9b418f88e94a99ae05848058600a
kazuma.nishimura <bbttbtbt000@gmail.com>
2021-04-25 13:46:54 +0000

    kadai-micropost

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -18,25 +18,16 @@
                         <ul class="dropdown-menu dropdown-menu-right">
                             {{-- ユーザ詳細ページへのリンク --}}
                             <li class="dropdown-item">{!! link_to_route('users.show', 'My profile', ['user' => Auth::id()]) !!}</li>
-                            
-                             {{-- ログインページへのリンク --}}
-                            <li class="dropdown-item">{!! link_to_route('users.favoritings', 'Favorites', ['id' => $user->id] )!!}</li>
-                            
                             <li class="dropdown-divider"></li>
                             {{-- ログアウトへのリンク --}}
                             <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
-                            
-                            
                         </ul>
                     </li>
                 @else
                     {{-- ユーザ登録ページへのリンク --}}
                     <li class="nav-item">{!! link_to_route('signup.get', 'Signup', [], ['class' => 'nav-link']) !!}</li>
                     {{-- ログインページへのリンク --}}
-                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
-                    
-                   
-                @endif
+                    <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>                @endif
             </ul>
         </div>
     </nav>
